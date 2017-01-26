4caab53573bbdde48171cd7fc506a1fce842f7d4
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-01-25 21:27:47 -0600

    Working code for 3 boxes submit button NO thanks

--- form/main.py
+++ form/main.py
@@ -1,24 +1,22 @@
-﻿#ULC video class2.5
+﻿#ULC video class2.5 only on GAEL and GIT
 
 import webapp2
 form="""
 <form method="post">
 	What is your birthday?
-	 <br>
-	 <label>Month <input type="text" name="month" ></label> <br><br>
-	 <label>Day <input type="text" name="day"></label><br><br>
-     <label>Year <input type="text" name="year"></label><br><br>
-  	 <br>
-	 <br>
-     <input type="submit">
+    <br>
+    <input type="text" name="month" > Month <br><br>
+    <input type="text" name="day"> Day <br><br>
+    <input type="text" name="year"> Year <br><br>
+    <br>
+    <br>
+    <input type="submit">
 </form>
 """
 class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(form)
+    def get(self):
+        self.response.out.write(form)
 
-    def post(self):
-        self.response.out.write("Thanks for a valid day !")
 
 app = webapp2.WSGIApplication([
 	('/',MainPage)
--- main.py
+++ main.py
@@ -1,49 +1,56 @@
-﻿#ULC video class2.5
+#ULC video class2.5
 
 import webapp2
-
 form="""
-
 <form method="post">
-    
 	What is your birthday?
-   
-	 <br>
-   
-	 <label>Month <input type="text" name="month" ></label> <br><br>
-  
-	 <label>Day <input type="text" name="day"></label><br><br>
-    
-	<label>Year <input type="text" name="year"></label><br><br>
-   
-  	 <br>
-   
-	 <br>
-    
-	<input type="submit">
-
+	<br>
+	<label>Month <input type="text" name="month" ></label> <br><br>
+	<label>Day <input type="text" name="day"></label><br><br>
+    <label>Year <input type="text" name="year"></label><br><br>
+  	<br>
+	<br>
+    <input type="submit">
 </form>
-
 """
-
-
 class MainPage(webapp2.RequestHandler):
-  
 	def get(self):
-       
-                		 #self.response.headerscontent-Type'] = 'text/plain'
-        
-		self.response.out.write(form)
-   
+        self.response.out.write(form)
 
-	 def post(self):
-        
-		self.response.out.write("Thanks for a valid day !")
-
-
+    def post(self):
+        self.response.out.write("Thanks for a valid day !")
 
 app = webapp2.WSGIApplication([
 	('/',MainPage)
 	], debug=True)
+'''
+#Failed code with error display
+#ULC video class2.5
+import webapp2
+form="""
+<form method="post">
+    What is your birthday?
+    <br>
+    <label>Month <input type="text" name="month" ></label> <br><br>
+    <label>Day <input type="text" name="day"></label><br><br>
+    <label>Year <input type="text" name="year"></label><br><br>
+    <div style="color":red>%(error)s</div>
+    <br>
+    <br>
+    <input type="submit">
+</form>
+"""
 
+class MainPage(webapp2.RequestHandler):
+    def write_form(self,error=""):
+        self.response.out.wrote(form%{"error":error})
+    def get(self):
+        #self.response.headerscontent-Type'] = 'text/plain'
+        self.write_form
+    def post(self):
+        if month=="January" :
+            self.write_form("Thanks for a valid day !")
+        else:
+            self.write_form("Invalid month")
 
+app = webapp2.WSGIApplication([('/',MainPage)], debug=True)
